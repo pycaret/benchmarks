a5c896363d462469a4584733fbbea85987538e64
Nikhil Gupta <mywork.ng@gmail.com>
2022-12-23 14:50:35 -0600

    removed unnecessary files

--- benchmarks/tests/test_badmodule.py
+++
@@ -1,5 +0,0 @@
-from benchmarks.badmodule import bad_function
-
-
-def test_bad_function():
-    assert bad_function(1) == 1
--- benchmarks/tests/test_examplemodule.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from benchmarks.examplemodule import example_function
-
-
-@pytest.mark.parametrize("test_input,expected", [(2, 4), (3, 9), (5, 25)])
-def test_example_function(test_input, expected):
-    assert example_function(test_input) == expected
