3ae40768bfc0285057351226a7f0371b182b7ec4
Nikhil Gupta <mywork.ng@gmail.com>
2023-01-21 17:48:20 -0600

    added scripts for property extraction per #24

---
+++ benchmarks/parallel/time_series/properties.py
@@ -0,0 +1,89 @@
+"""Module to extract the time series properties."""
+import logging
+import time
+from typing import Optional
+
+import pandas as pd
+from pycaret.time_series import TSForecastingExperiment
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(message)s")
+
+
+def extract_properties(
+    data: pd.DataFrame,
+    setup_kwargs: dict,
+    prefix: Optional[str] = None,
+) -> pd.DataFrame:
+    """Extracts the properties of a single time series using the setup settings.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        The data for a single intersection or group (i.e. unique_id)
+    setup_kwargs : dict
+        The arguments to pass to pycaret's setup function. The following arguments
+        are auto inferred and do not need to be included:
+        (1) data: taken from the data argument of this function
+        (2) experiment_name: derived from the unique_id in the data and the prefix.
+    prefix : str
+        Prefix to use for the experiment name, by default None
+
+    Returns
+    -------
+    pd.DataFrame
+        Properties of the time series
+    """
+    unique_id = data["unique_id"].unique()[0]
+    prefix = prefix or "dataset"
+    properties = pd.DataFrame()
+    setup_passed = False
+    try:
+        start = time.time()
+        exp = TSForecastingExperiment()
+        exp.setup(data=data, experiment_name=f"{prefix}_{unique_id}", **setup_kwargs)
+        end = time.time()
+        setup_passed = True
+        try:
+            properties = pd.DataFrame(
+                {
+                    "len_total": [len(exp.y)],
+                    "len_train": [len(exp.y_train)],
+                    "len_test": [len(exp.y_test)],
+                    "strictly_positive": [exp.strictly_positive],
+                    "white_noise": [exp.white_noise],
+                    "lowercase_d": [exp.lowercase_d],
+                    "uppercase_d": [exp.uppercase_d],
+                    "seasonality_present": [exp.seasonality_present],
+                    "candidate_sps": [exp.candidate_sps],
+                    "significant_sps": [exp.significant_sps],
+                    "all_sps": [exp.all_sps_to_use],
+                    "primary_sp": [exp.primary_sp_to_use],
+                    "significant_sps_no_harmonics": [exp.significant_sps_no_harmonics],
+                    "all_sps_no_harmonics": [
+                        exp.significant_sps_no_harmonics[0 : len(exp.all_sps_to_use)]
+                    ],
+                    "primary_sp_no_harmonics": [exp.significant_sps_no_harmonics[0]],
+                    "time_taken": [round(end - start, 4)],
+                }
+            )
+        except Exception as e:
+            logging.warn(
+                f"Error occurred for ID: {unique_id} when extracting properties: "
+                f"{e}"
+            )
+    except Exception as e:
+        if not setup_passed:
+            logging.warn(
+                f"Error occurred for ID: {unique_id} during experiment setup. "
+                f"No properties extracted created: {e}"
+            )
+
+    # Fugue does not return back the group by column (like Pandas)
+    # Hence, add it as a column
+    properties["unique_id"] = unique_id
+
+    # Fugue may have issues handling index.
+    # Hence it is better to reset it before returning.
+    properties.reset_index(inplace=True)
+
+    return properties
---
+++ scripts/mseries/extract_properties.py
@@ -0,0 +1,257 @@
+"""
+Execution command (examples):
+>>> python scripts/mseries/extract_properties.py --help
+>>> python scripts/mseries/extract_properties.py --dataset=M3 --group=Other --execution_mode=native --execution_engine=ray
+"""
+
+import logging
+import multiprocessing as mp
+import os
+import sys
+from datetime import date
+from typing import Optional
+
+import fire
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+from benchmarks.datasets.create.time_series.mseries import get_data
+from benchmarks.parallel.execution import (
+    execute,
+    initialize_engine,
+    run_execution_checks,
+    shutdown_engine,
+)
+from benchmarks.parallel.time_series.properties import extract_properties
+from benchmarks.utils import _return_dirs, _return_pycaret_version_or_hash
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(message)s")
+
+# Register `pandas.progress_apply` and `pandas.Series.map_apply` with `tqdm`
+tqdm.pandas()
+
+
+def main(
+    dataset: str = "M3",
+    group: str = "Other",
+    execution_mode: str = "native",
+    execution_engine: str = "ray",
+    num_cpus: Optional[int] = None,
+) -> None:
+    """Benchmark the performance of a single model across multiple individual
+    time series.
+
+    Parameters
+    ----------
+    dataset : str, optional
+        Dataset to benchmark, by default "M3"
+        NOTE: Currently only M3 is supported
+    group : str, optional
+        Options: "Yearly", "Quarterly", "Monthly", "Other", by default "Other"
+    execution_mode : str, optional
+        Should the execution be done natively or using the Fugue wrapper
+        Options: "native", "fugue", by default "native"
+    execution_engine : str, optional
+        What engine should be used for executing the code.
+        Options: "local", "ray", "spark", by default "ray"
+            - "local" will execute serially using pandas
+            - "ray" will execute in parallel using Ray
+            - "spark" will execute in parallel using Spark
+        NOTE: Currently only "local" and "ray" are supported
+    num_cpus : Optional[int], optional
+        Number of CPUs to use to execute in parallel, by default None which uses
+        up all available CPUs. In local mode, this is ignored and only 1 CPU is
+        used .
+
+    Raises
+    ------
+    ValueError
+        (1) execution_mode is not one of "native" or "fugue"
+        (2) engine is not one of "local", "ray", or "spark"
+        (3) engine is not implemented
+    """
+
+    EXEC_MODE_VERSION, EXEC_ENGINE_VERSION = run_execution_checks(
+        execution_mode, execution_engine
+    )
+
+    logging.info("\n\n")
+    LIBRARY = "pycaret"
+    LIBRARY_VERSION = _return_pycaret_version_or_hash()
+    OS = sys.platform
+    PYTHON_VERSION = sys.version.split()[0]
+    RUN_DATE = date.today().strftime("%Y-%m-%d")
+    num_cpus = num_cpus or mp.cpu_count()
+    logging.info(
+        f"\nRun Date: {RUN_DATE}"
+        f"\nExtracting features for Dataset: '{dataset}' Group: '{group}' using ..."
+        f"\n  - OS: '{OS}'"
+        f"\n  - Python Version: '{PYTHON_VERSION}'"
+        f"\n  - Library: '{LIBRARY}' Version: '{LIBRARY_VERSION}'"
+        f"\n  - Execution Engine: '{execution_engine}' Version: '{EXEC_ENGINE_VERSION}'"
+        f"\n  - Execution Mode: '{execution_mode}' Version: '{EXEC_MODE_VERSION}'"
+        f"\n  - CPUs: {num_cpus}"
+    )
+
+    keys = [
+        dataset,
+        group,
+        LIBRARY,
+        LIBRARY_VERSION,
+        execution_engine,
+        EXEC_ENGINE_VERSION,
+        execution_mode,
+        EXEC_MODE_VERSION,
+        num_cpus,
+        PYTHON_VERSION,
+        OS,
+    ]
+
+    prefix = "-".join([str(key) for key in keys])
+
+    # -------------------------------------------------------------------------#
+    # Get the data ----
+    # -------------------------------------------------------------------------#
+    BASE_DIR, _, _, PROPERTIES_DIR = _return_dirs(dataset=dataset)
+
+    # Check if the directory exists. If not, create it.
+    for dir in [PROPERTIES_DIR]:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+    train, fh, _, _ = get_data(directory=BASE_DIR, dataset=dataset, group=group)
+    test, _, _, _ = get_data(
+        directory=BASE_DIR, dataset=dataset, group=group, train=False
+    )
+
+    # We only need the y_test time points. y_test values should be unknown to
+    # avoid any chance of leakage
+    test["y"] = np.nan
+
+    combined = pd.concat([train, test], axis=0)
+    combined["ds"] = pd.to_datetime(combined["ds"])
+
+    # # For local testing on a small subset ----
+    # all_ts = combined["unique_id"].unique()
+    # combined = combined[combined["unique_id"].isin(all_ts[:2])]
+
+    # -------------------------------------------------------------------------#
+    # Experiment Settings ----
+    # NOTE:
+    # (1) Test only has time points. No values. This is to avoid leakage.
+    # (2) Only use the train portion to extract features since test will be imputed.
+    # -------------------------------------------------------------------------#
+
+    verbose = False
+    setup_kwargs = {
+        "fh": fh,
+        "target": "y",
+        "index": "ds",
+        "fold": 1,
+        "numeric_imputation_target": "ffill",
+        "hyperparameter_split": "train",
+        "ignore_features": ["unique_id"],
+        "n_jobs": 1,
+        "session_id": 42,
+        "verbose": verbose,
+    }
+    apply_kwargs = dict(prefix=prefix, setup_kwargs=setup_kwargs)
+
+    # Fugue required the schema of the returned dataframe ----
+    if execution_mode == "fugue":
+        schema = (
+            "len_total : int"
+            ",len_train : int"
+            ",len_test : int"
+            ",strictly_positive : bool"
+            ",white_noise : str"
+            ",lowercase_d : int"
+            ",uppercase_d : int"
+            ",seasonality_present :bool"
+            ",candidate_sps :list"
+            ",significant_sps :list"
+            ",all_sps :list"
+            ",primary_sp :int"
+            ",significant_sps_no_harmonics :list"
+            ",all_sps_no_harmonics :list"
+            ",primary_sp_no_harmonics :int"
+            ",time_taken :float"
+        )
+    else:
+        schema = None
+
+    # -------------------------------------------------------------------------#
+    # Run Benchmarking ----
+    # -------------------------------------------------------------------------#
+    initialize_engine(execution_engine, num_cpus)
+    properties, time_taken = execute(
+        all_groups=combined,
+        keys="unique_id",
+        function_single_group=extract_properties,
+        function_kwargs=apply_kwargs,
+        execution_mode=execution_mode,
+        execution_engine=execution_engine,
+        num_cpus=num_cpus,
+        schema=schema,
+    )
+    shutdown_engine(execution_engine)
+
+    # -------------------------------------------------------------------------#
+    # Write Results ----
+    # -------------------------------------------------------------------------#
+
+    # Order columns (as different execution engines may have different orders) ----
+    cols = [
+        "unique_id",
+        "len_total",
+        "len_train",
+        "len_test",
+        "strictly_positive",
+        "white_noise",
+        "lowercase_d",
+        "uppercase_d",
+        "seasonality_present",
+        "candidate_sps",
+        "significant_sps",
+        "all_sps",
+        "primary_sp",
+        "significant_sps_no_harmonics",
+        "all_sps_no_harmonics",
+        "primary_sp_no_harmonics",
+        "time_taken",
+    ]
+    properties = properties[cols]
+
+    # Write results ----
+    properties_file_name = f"{PROPERTIES_DIR}/properties-{prefix}.csv"
+    logging.info(f"Writing properties to {properties_file_name}")
+    properties.to_csv(properties_file_name, index=False)
+
+    # Add all KEY_COLS along with Non Static columns like time and run date ----
+    time_df = pd.DataFrame(
+        {
+            "dataset": [dataset],
+            "group": [group],
+            "library": [LIBRARY],
+            "library_version": [LIBRARY_VERSION],
+            "execution_engine": [execution_engine],
+            "execution_engine_version": [EXEC_ENGINE_VERSION],
+            "execution_mode": [execution_mode],
+            "execution_mode_version": [EXEC_MODE_VERSION],
+            "num_cpus": [num_cpus],
+            "python_version": [PYTHON_VERSION],
+            "os": [OS],
+            "time": [time_taken],
+            "run_date": [RUN_DATE],
+        }
+    )
+    time_file_name = f"{PROPERTIES_DIR}/time-{prefix}.csv"
+    logging.info(f"Writing time to {time_file_name}")
+    time_df.to_csv(time_file_name, index=False)
+
+    logging.info("Property Extraction Complete!")
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
